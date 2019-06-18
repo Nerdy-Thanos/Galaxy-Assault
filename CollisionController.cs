27f1fe0d992f81fb89b145255fb700e8ada09de6
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2019-06-18 14:58:38 +0530

    collision controller script added

---
+++ CollisionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionController : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathFX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
